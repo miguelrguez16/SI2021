c460d9dfab8a521870463ede69ef408471734358
Miguel <miguelrguez16@users.noreply.github.com>
2021-03-31 15:50:21 +0200

    Historia 3 miguel finalizada

---
+++ src/main/java/giis/demo/nuevosCursos/NuevosCursoDisplayDTO.java
@@ -0,0 +1,87 @@
+package giis.demo.nuevosCursos;
+
+public class NuevosCursoDisplayDTO {
+	private String idCurso, nombre, precio, fechaInicio, fechaFin, precioPrecolegiado, precioColectivo, profesor, instalacion, sesiones;
+
+	public String getIdCurso() {
+		return idCurso;
+	}
+
+	public void setIdCurso(String idCurso) {
+		this.idCurso = idCurso;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
+
+	public String getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(String fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public String getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(String fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+
+	public String getPrecioPrecolegiado() {
+		return precioPrecolegiado;
+	}
+
+	public void setPrecioPrecolegiado(String precioPrecolegiado) {
+		this.precioPrecolegiado = precioPrecolegiado;
+	}
+
+	public String getPrecioColectivo() {
+		return precioColectivo;
+	}
+
+	public void setPrecioColectivo(String precioColectivo) {
+		this.precioColectivo = precioColectivo;
+	}
+
+	public String getProfesor() {
+		return profesor;
+	}
+
+	public void setProfesor(String profesor) {
+		this.profesor = profesor;
+	}
+
+	public String getInstalacion() {
+		return instalacion;
+	}
+
+	public void setInstalacion(String instalacion) {
+		this.instalacion = instalacion;
+	}
+
+	public String getSesiones() {
+		return sesiones;
+	}
+
+	public void setSesiones(String sesiones) {
+		this.sesiones = sesiones;
+	}
+	
+	
+}
---
+++ src/main/java/giis/demo/nuevosCursos/NuevosCursoModel.java
@@ -0,0 +1,39 @@
+package giis.demo.nuevosCursos;
+
+import java.util.Date;
+import java.util.List;
+import giis.demo.util.Database;
+import giis.demo.util.Util;
+
+public class NuevosCursoModel {
+
+	private Database db = new Database();
+
+	public List<NuevosCursoDisplayDTO> getListaCursosModelo() {
+		String sql = "SELECT idCurso, nombre,precio,fechaInicio,fechaFin,precioPrecolegiado,precioColectivo,profesor,instalacion,sesiones "
+				+ "FROM CURSO " + "WHERE estado ='planificado'";
+		return db.executeQueryPojo(NuevosCursoDisplayDTO.class, sql);
+	}
+	
+	
+	public boolean existeCurso(String nombreCurso) {
+		String sql = "SELECT idCurso, nombre "
+				+ "FROM CURSO " 
+				+ "WHERE (estado ='planificado' or estado ='abierto') and nombre= ?";
+		List<NuevosCursoDisplayDTO> cursosNombre = db.executeQueryPojo(NuevosCursoDisplayDTO.class, sql, nombreCurso);
+		return !cursosNombre.isEmpty();
+	}
+
+	/* 
+	 * Introduce un nuevo curso en la base de datos
+	 */
+	public void setCursoNuevos(String nombre, double precio, Date fechaInicio, Date fechaFin, double precioPrecolegiado,
+			double precioColectivo, String profesor, String instalacion, int sesiones) {
+		String sql = "Insert into Curso (nombre, precio, fechaInicio,fechaFin,precioPrecolegiado,precioColectivo, profesor,instalacion,sesiones) VALUES"
+				+ "(?,?,?,?,?,?,?,?,?)";
+		db.executeUpdate(sql, nombre, precio, Util.dateToIsoString(fechaInicio), Util.dateToIsoString(fechaFin),
+				precioPrecolegiado, precioColectivo, profesor, instalacion, sesiones);
+	}
+
+}
+// ("idCurso","nombre","precio","plazasTotales","fechaInicio","fechaFin","fechaInicioInscripcion","fechaFinInscripción","estado","precioPrecolegiado","precioColectivo","profesor","instalacion","sesiones")
