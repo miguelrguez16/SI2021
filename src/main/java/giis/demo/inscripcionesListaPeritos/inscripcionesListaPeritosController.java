e74c4f2bc76cb1cb4008137527906c4aae6a7cc8
maria <maria@LAPTOP-NA6E6AU6>
2021-04-11 12:20:57 +0200

    fin comprobacion historia 5 maria

--- src/main/java/giis/demo/inscripcionesListaPeritos/inscripcionesListaPeritosController.java
+++ src/main/java/giis/demo/inscripcionesListaPeritos/inscripcionesListaPeritosController.java
@@ -25,8 +25,9 @@ public class inscripcionesListaPeritosController {
 	public void initController() {
 		vista.getBtnVerDatos().addActionListener(e -> SwingUtil.exceptionWrapper(() -> visualizarDatos()));
 
-		vista.getBtnInscripcionPerito()
-				.addActionListener(e -> SwingUtil.exceptionWrapper(() -> generarInscripcionPerito()));
+		vista.getBtnInscripcionPerito().addActionListener(e -> SwingUtil.exceptionWrapper(() -> generarInscripcionPerito()));
+		
+		vista.getBtnReset().addActionListener(e -> SwingUtil.exceptionWrapper(() -> reset()));
 	}
 
 	private void visualizarDatos() {
@@ -39,8 +40,9 @@ public class inscripcionesListaPeritosController {
 					"Error en el formato del idColegiado: " + vista.getTFidColegiado().getText() + " no válido.");
 		} finally {
 			aux = modelo.getColegiadoNombre(idColegiado) == null ? "" : modelo.getColegiadoNombre(idColegiado);
-			System.out.println("Id colegiado elegido: " +idColegiado);
 			if (!aux.equals("")) {
+				System.out.println("Id colegiado elegido: " +idColegiado);
+				vista.getLid().setText(vista.getTFidColegiado().getText());
 				vista.getTFnombre().setText(modelo.getColegiadoNombre(idColegiado));
 				vista.getTFapellidos().setText(modelo.getColegiadoApellidos(idColegiado));
 				vista.getTFdireccion().setText(modelo.getColegiadoDireccion(idColegiado));
@@ -52,31 +54,27 @@ public class inscripcionesListaPeritosController {
 				vista.getTFcentro().setText(modelo.getColegiadoCentro(idColegiado));
 				vista.getTFanio().setText(modelo.getColegiadoAnioT(idColegiado));
 				vista.getTFdni().setText(modelo.getColegiadoDNI(idColegiado));
+				vista.getTFidColegiado().setEnabled(false);
+				vista.getBtnVerDatos().setEnabled(false);
+				vista.getBtnInscripcionPerito().setEnabled(true);
 			} else {
+				vista.getTFnombre().setText("");
+				vista.getTFapellidos().setText("");
+				vista.getTFdireccion().setText("");
+				vista.getTFpoblacion().setText("");
+				vista.getTFtelefono().setText("");
+				vista.getTFdatos().setText("");
+				vista.getTFfecha().setText("");
+				vista.getTFtitulacion().setText("");
+				vista.getTFcentro().setText("");
+				vista.getTFanio().setText("");
+				vista.getTFdni().setText("");
+				vista.getBtnVerDatos().setEnabled(true);
+				vista.getBtnInscripcionPerito().setEnabled(false);
 				throw new ApplicationException(	"El idColegiado=" + idColegiado + " no existe en nuestra Base de Datos.");
-
 			}
 		}
-
 	}
-	// idColegiado=modelo.getColegiadoNombre(idColegiado)==null ? -1 :
-	// Integer.parseInt(modelo.getColegiadoNombre(idColegiado));
-	/*
-	 * if(idColegiado!=-1) {
-	 * vista.getTFnombre().setText(modelo.getColegiadoNombre(idColegiado));
-	 * vista.getTFapellidos().setText(modelo.getColegiadoApellidos(idColegiado));
-	 * vista.getTFdireccion().setText(modelo.getColegiadoDireccion(idColegiado));
-	 * vista.getTFpoblacion().setText(modelo.getColegiadoPoblacion(idColegiado));
-	 * vista.getTFtelefono().setText(modelo.getColegiadoTelefono(idColegiado));
-	 * vista.getTFdatos().setText(modelo.getColegiadoDatos(idColegiado));
-	 * vista.getTFfecha().setText(modelo.getColegiadoFechaS(idColegiado));
-	 * vista.getTFtitulacion().setText(modelo.getColegiadoTitulacion(idColegiado));
-	 * vista.getTFcentro().setText(modelo.getColegiadoCentro(idColegiado));
-	 * vista.getTFanio().setText(modelo.getColegiadoAnioT(idColegiado));
-	 * vista.getTFdni().setText(modelo.getColegiadoDNI(idColegiado)); } else throw
-	 * new ApplicationException("El idColegiado="
-	 * +idColegiado+" no existe en nuestra Base de Datos."); }
-	 */
 
 	private void generarInscripcionPerito() {
 
@@ -84,6 +82,9 @@ public class inscripcionesListaPeritosController {
 		if (modelo.existePerito(idColegiado)) {
 			throw new ApplicationException("Ya hay un perito con ese idColegiado.");
 		} else {
+			if(vista.getTFnombre().getText().isEmpty()) {
+				throw new ApplicationException("Los datos del colegiado deben estar rellenados.");
+			}
 			justificante.getFrame().setVisible(true);
 			modelo.actualizarColegiadoDatos(idColegiado);
 			modelo.setNuevoPerito(idColegiado);
@@ -95,6 +96,24 @@ public class inscripcionesListaPeritosController {
 			justificante.getLturno().setText(modelo.getPeritoTurno(idColegiado));
 			System.out.println("Inscripcion en Perito: idColegiado=" + idColegiado);
 		}
-
+	}
+	
+	private void reset() {
+		vista.getLid().setText("");
+		vista.getTFidColegiado().setEnabled(true);
+		vista.getTFidColegiado().setText("");
+		vista.getTFnombre().setText("");
+		vista.getTFapellidos().setText("");
+		vista.getTFdireccion().setText("");
+		vista.getTFpoblacion().setText("");
+		vista.getTFtelefono().setText("");
+		vista.getTFdatos().setText("");
+		vista.getTFfecha().setText("");
+		vista.getTFtitulacion().setText("");
+		vista.getTFcentro().setText("");
+		vista.getTFanio().setText("");
+		vista.getTFdni().setText("");
+		vista.getBtnVerDatos().setEnabled(true);
+		vista.getBtnInscripcionPerito().setEnabled(false);
 	}
 }
